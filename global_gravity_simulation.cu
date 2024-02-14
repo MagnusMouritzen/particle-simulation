12a42ef89c084d051d5d33786453b13f2a65f4f9
Magnus <magnus.mouritzen@gmail.com>
2024-02-14 12:14:25 +0100

    Basic global gravity

--- global_gravity_simulation.cu
+++ global_gravity_simulation.cu
@@ -11,30 +11,40 @@ struct Electron {
         float3 velocity;
 };
 
-__global__ static void update(Electron* electrons) {
+__global__ static void update(Electron* electrons, float deltaTime) {
     int i = threadIdx.x;
-
+    electrons[i].velocity.y -= 9.82 * deltaTime;
+    electrons[i].position.y += electrons[i].velocity.y * deltaTime;
+    if (electrons[i].position.y <= 0){
+        electrons[i].position.y = -electrons[i].position.y;
+        electrons[i].velocity.y = -electrons[i].velocity.y;
+    }
 }
 
 void globalGravityRun(int N) {
 
     Electron* electrons_host = (Electron *)malloc(N * sizeof(Electron));
     for(int i=0; i<N; i++) {
-        electrons_host[i].position = make_float3(randomFloat(0,5), randomFloat(0,5), 1.0);
-        electrons_host[i].velocity = make_float3(0.0, 1.0, 0.0);
+        electrons_host[i].position = make_float3(randomFloat(0,5), randomFloat(10,50), 1.0);
         electrons_host[i].weight = 1.0;
     }
-    printf("%.6f \n", electrons_host[0].position.y);
 
     Electron* electrons;
     cudaMalloc(&electrons, N * sizeof(Electron));
 
     cudaMemcpy(electrons, electrons_host, N * sizeof(Electron), cudaMemcpyHostToDevice);
-    
-    update<<<1, N>>>(electrons);
 
-    cudaMemcpy(electrons_host, electrons, N * sizeof(Electron), cudaMemcpyDeviceToHost);
+    printf("Time %d, position %.6f, velocity %.6f\n", 0, electrons_host[0].position.y, electrons_host[0].velocity.y);
+    for (int i = 1; i < 101; i++){
+        update<<<1, N>>>(electrons, 0.1);
+
+        if (i % 5 == 0){
+            cudaMemcpy(electrons_host, electrons, N * sizeof(Electron), cudaMemcpyDeviceToHost);
+            printf("Time %d, position %.6f, velocity %.6f\n", i, electrons_host[0].position.y, electrons_host[0].velocity.y);
+        }
+    }
+
     
-    printf("%.6f \n", electrons_host[0].position.y);
+
     
 }

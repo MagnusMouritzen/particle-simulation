bb827773fd7afc94f590258d53d8082cdaf8d898
Johannes Christian Poulsen <s204399@student.dtu.dk>
2024-02-19 12:46:44 +0100

    to gif python

---
+++ to_gif.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import glob
+
+# Create the frames
+frames = []
+imgs = glob.glob("out/*.png")
+for i in sorted(imgs):
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+# Save into a GIF file that loops forever
+frames[0].save('out/result.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=300, loop=0)

eaed5be77736917d550fa02a89c97ae9c7e13db8
Magnus <magnus.mouritzen@gmail.com>
2024-04-09 14:27:51 +0200

    Updated cell

--- src/cell.h
+++ src/cell.h
@@ -1,10 +1,13 @@
+float cell_size = 8.3e-3;
+int3 grid_size = make_int3(512, 512, 512);
+float3 sim_size = make_float3(grid_size.x * cell_size, grid_size.y * cell_size, grid_size.z * cell_size);
+double epsilon0 = 8.8541878176E-12;
+double pi = 3.1415926536;
+double electric_force_constant = 1 / (4 * pi * epsilon0 * cell_size * cell_size);
 
-inline int cell_size = 8.3e-3;
-inline int3 grid_size = make_int3(512, 512, 512);
-inline float3 sim_size = make_float3(grid_size.x * 8.3e-3, grid_size.y * 8.3e-3, grid_size.z * 8.3e-3);
 
 struct Cell {
     public:
         double charge;
-        float3 forces;
+        double3 acceleration;
 };

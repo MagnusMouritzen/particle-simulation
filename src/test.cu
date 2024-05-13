14a634820b5c64b27dc5d3e6bcfa0a3a3ea0b643
Magnus <magnus.mouritzen@gmail.com>
2024-05-13 16:53:52 +0200

    Test poisson timesteps

--- src/test.cu
+++ src/test.cu
@@ -3,35 +3,39 @@ using namespace std;
 
 void runBenchmark(){
     vector<TimingData> data;
-    int init_ns[] = {100000};
+    int init_ns[] = {1000000};
     int block_sizes[] = {128,256,512,1024};
-    int max_ts[] = {10000};
+    int mobility_stepss[] = {1000};
     int max_ns[] = {100000000};
     int functions[] = {0,1,2,3};
     int sleep_times[] = {100};
+    int poisson_stepss[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    for(int init_n : init_ns) {
-        for(int block_size : block_sizes){
-            for(int max_t : max_ts){
-                for(int max_n : max_ns){
-                    for(int sleep_time : sleep_times){
-                        for(int function : functions){
-                            RunData run_data = runPIC(init_n, max_n, max_t, 1, function, 0, block_size, sleep_time);
-                            if (run_data.final_n >= max_n) {
-                                //throw runtime_error("Illegal configuration, capacity reached!");
-                                printf("\n\n\nIllegal!!!\n\n\n");
-                                continue;
+    for (int mobility_steps : mobility_stepss){
+        for(int init_n : init_ns) {
+            for(int block_size : block_sizes){
+                for(int poisson_steps : poisson_stepss){
+                    for(int max_n : max_ns){
+                        for(int sleep_time : sleep_times){
+                            for(int function : functions){
+                                if (function == 2 && block_size > 256) continue;
+                                RunData run_data = runPIC(init_n, max_n, poisson_steps, mobility_steps, function, 0, block_size, sleep_time);
+                                if (run_data.final_n >= max_n) {
+                                    //throw runtime_error("Illegal configuration, capacity reached!");
+                                    printf("\n\n\nIllegal!!!\n\n\n");
+                                    continue;
+                                }
+                                data.push_back(run_data.timing_data);
+                                free(run_data.electrons);
                             }
-                            data.push_back(run_data.timing_data);
-                            free(run_data.electrons);
+                            
                         }
-                        
                     }
                 }
             }
         }
     }
-    printCSV(data, "out/data/data.csv");
+    printCSV(data, "out/data/poisson_timesteps.csv");
 }
 
 void runUnitTest(int init_n, int max_n, int max_t, int poisson_timestep, int verbose, int block_size, int sleep_time){
